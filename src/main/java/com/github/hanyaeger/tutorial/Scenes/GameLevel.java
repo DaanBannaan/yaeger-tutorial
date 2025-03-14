1e4be677fe49c7fb08f4256f1daf120e2c531b9d
DaanBannaan <DWM.Tijssen@student.han.nl>
2025-03-14 14:17:42 +0100

    h6 en 7

--- src/main/java/com/github/hanyaeger/tutorial/Scenes/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/Scenes/GameLevel.java
@@ -1,6 +1,9 @@
 package com.github.hanyaeger.tutorial.Scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.tutorial.entities.Hanny;
+import com.github.hanyaeger.tutorial.entities.Swordfish;
 
 public class GameLevel extends DynamicScene {
 
@@ -12,6 +15,7 @@ public class GameLevel extends DynamicScene {
 
     @Override
     public void setupEntities() {
-
+        addEntity(new Swordfish(new Coordinate2D(600, 400)));
+        addEntity(new Hanny(new Coordinate2D(100, 100)));
     }
 }
